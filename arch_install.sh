5b53c78727262712410d749ddd00fa1fb3f1bf1e
gitalyx <159016936+gitalyx@users.noreply.github.com>
2024-05-14 11:07:46 +0300

    make the installation faster

---
+++ arch_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Do you want to install: Hyprland, Polybar, Waybar, Swaybg Alacritty, Pcmanfm, Rofi, Neovim and Neofetch?"
+
+echo "No(n) or Yes(y):"
+
+echo
+    read -n 1 -s -r -p "Press 'y' to continue or 'n' to exit: " key
+    echo
+    case "$key" in
+        [Yy]) echo "Continuing...";;
+        [Nn]) echo "Exiting."; exit;;
+        *) echo "Invalid input. Press 'y' to continue or 'n' to exit."; pause;;
+    esac
+
+sudo pacman -Syu 
+sudo pacman -S hyprland polybar waybar alacritty rofi neovim neofetch pcmanfm swaybg
+
+
+echo "Do you want to copy the configurations to ~/.config and rewrite .bashrc"
+
+
+echo
+    read -n 1 -s -r -p "Press 'y' to continue or 'n' to exit: " key
+    echo
+    case "$key" in
+        [Yy]) echo "Continuing...";;
+        [Nn]) echo "Exiting."; exit;;
+        *) echo "Invalid input. Press 'y' to continue or 'n' to exit."; pause;;
+    esac
+
+cp alacritty ~/.config/
+cp gtk-3.0 ~/.config/
+cp hypr ~/.config/
+cp neofetch ~/.config/
+cp nvim ~/.config/
+cp pcman/default ~/.config/
+cp rofi ~/.config/
+cp swaybg ~/.config/
+cp wallpapers ~/.config/
+cp .bashrc ~/
+
+echo "Setup completed successfully."
